89f3fe1b7bd27ec55bef2b037e56c2098301ed5b
fab33150 <fab33150@gmail.com>
2019-12-03 14:16:01 -0500

    Updated Design and Color labels

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ $("#name").focus();
 
 //Hide "Other" job field option text input
 $("#other-title").hide();
+
+//Hides the  'Select Theme' option on the Design menu
+$("option[value='Select Theme']").attr('disabled', true).attr('hidden', true);
+
+//Updates 'Color' field to read 'Please Select a T-shirt theme'
+$('#colors-js-puns label').text("Please Select T-shirt theme");
