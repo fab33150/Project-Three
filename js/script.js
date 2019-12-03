51a87707b1a422af385e9f85c04ac23580da43cc
fab33150 <fab33150@gmail.com>
2019-12-03 16:31:07 -0500

    Working on Design theme selection matching with the right color options

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ $("option[value='Select Theme']").attr('disabled', true).attr('hidden', true);
 
 //Updates 'Color' field to read 'Please Select a T-shirt theme'
 $('#colors-js-puns label').text("Please Select T-shirt theme");
+
+//Hides the 'Color' dropdown options
+$('#color option').hide();
+
+//Variables for the Design Options and Color Options
+var $updateDesign = $('#design');
+var $updateDesignOptions = $('#design option');
+var $colorOptions = $('#color option');
+//console.log($colorOptions);
+
+$updateDesign.on('change', function(event){
+    $updateDesignOptions.each(function(i, element){
+        if ($(event.target).val() === 'js-puns'){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
